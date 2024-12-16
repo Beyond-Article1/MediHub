2ce99e7878dd4bac2538cdf5e751f63c8586364f
JIYOUNG-22 <dyong0922@naver.com>
2024-12-16 09:23:29 +0900

    feat: #7 채팅 기본 화면 개발

--- medihub-fe/src/App.vue
+++ medihub-fe/src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref } from 'vue';
 import Header from "@/components/common/Header.vue";
-import CpMain from "@/views/cp/CpMain.vue";
+import ChatButton from "@/components/chat/ChatButton.vue";
+import ChatWindow from "@/views/chat/ChatWindow.vue";
+
+const isChatOpen = ref(false); // 채팅창 열기/닫기 상태 관리
+
+const toggleChatWindow = () => {
+  isChatOpen.value = !isChatOpen.value;
+};
 </script>
 
 <template>
   <Header/>
-  <CpMain/>
+  <div id="app">
+    <RouterView/>
+    <ChatButton @toggle-chat-window="toggleChatWindow" />
+    <ChatWindow v-if="isChatOpen" :isOpen="isChatOpen" @close-chat-window="toggleChatWindow" />
+  </div>
 </template>
 
 <style scoped>
