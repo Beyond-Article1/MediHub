6eba029805db3bd0609e1abec5905be12d27c677
Pangtaek <zombie3632@gmail.com>
2024-12-16 09:15:43 +0900

    feat: #3 CP 리스트 개발

--- medihub-fe/src/components/cp/CpBody.vue
+++ medihub-fe/src/components/cp/CpBody.vue
@@ -1,13 +1,17 @@
 <script setup>
 import CpSearchCategory from "@/components/cp/CpSearchCategory.vue";
+import CpList from "@/components/cp/CpList.vue";
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <CpSearchCategory/>
+    <CpList/>
   </div>
 </template>
 
 <style scoped>
-
+.container{
+  display: flex;
+}
 </style>
---
+++ medihub-fe/src/components/cp/CpList.vue
@@ -0,0 +1,42 @@
+<script setup>
+import axios from "axios";
+import {onMounted, ref} from "vue";
+
+const authStore = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NSIsInVzZXJTZXEiOjQsInVzZXJOYW1lIjoiMTIzNDUiLCJhdXRoIjpbIlVTRVIiXSwiZXhwIjoxNzM0MDAxMzc0fQ.SApTOTHaBDihNtQ9N7mJovnq4u6p3kHekX48gKi2TQfR7BOI9AT4B9BP3o0boE5AxDz3tmKYEDUziOtPLxShJQ";
+
+const cpList = ref([]);
+
+async function fetchData() {
+  try {
+    const response = await axios.get("cp", {
+      headers: {
+        Authorization: `Bearer ${authStore}`
+      }
+    });
+
+    if (response.status === 200) {
+      console.log("CP 리스트 조회 성공");
+      console.log(response.data);
+      cpList.value = response.data;
+    } else {
+      console.log("CP 조회 실패");
+      console.log(`http status = ` + response.status);
+    }
+  } catch (error) {
+    console.error("예기치 못한 오류가 발생했습니다. 에러: \n", error);
+  }
+}
+
+onMounted(() => {
+  fetchData();
+})
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
