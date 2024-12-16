8db9f11fff477eeab8314f932ac1eb817ac402d8
Pangtaek <zombie3632@gmail.com>
2024-12-16 10:35:10 +0900

    feat: 부트스트랩 아이콘 호출

--- medihub-fe/src/main.js
+++ medihub-fe/src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 import axios from "axios";
 import {createPinia} from "pinia";
 import './styles/reset.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const app = createApp(App);
 const pinia = createPinia();
