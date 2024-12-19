f6d9c12a702f9e2c473700e1763a492827d43272
Pangtaek <zombie3632@gmail.com>
2024-12-19 22:51:45 +0900

    feat: #3 CP 의견 투표 기능 개발

--- medihub-fe/src/views/cp/cp-opinion/CpOpinionDetail.vue
+++ medihub-fe/src/views/cp/cp-opinion/CpOpinionDetail.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref, onMounted, computed} from "vue";
 import {useRoute, useRouter} from "vue-router";
+import {useAuthStore} from "@/store/authStore.js";
 
 import axios from "axios";
 
@@ -8,12 +9,12 @@ import KeywordList from "@/components/case_sharing/case_sharing/KeywordList.vue"
 import CaseContent from "@/components/case_sharing/case_sharing/CaseSharingContent.vue";
 import ContentTable from "@/components/case_sharing/case_sharing/ContentTable.vue";
 import LocalDateTimeFormat from "@/components/common/LocalDateTimeFormat.vue";
-import IconButton from "@/components/common/button/IconButton.vue";
 import SvgIconButton from "@/components/common/button/svgIconButton.vue";
 
 // vue 설정 변수
 const route = useRoute();
 const router = useRouter();
+const authStore = useAuthStore();
 
 // 데이터 저장 변수
 const cpOpinion = ref([]);      // 조회된 CP 의견
@@ -21,6 +22,7 @@ const jsonCpOpinion = ref([]);  // JSON 형식으로 저장한 CP 의견
 
 onMounted(() => {
   fetchData();
+  console.log(`/cp/${route.params.cpVersionSeq}/cpOpinionLocation/${route.params.cpOpinionLocationSeq}/${route.params.cpOpinionSeq}/cpOpinionVote`);
 })
 
 // 데이터 호출 함수
@@ -87,6 +89,53 @@ function moveToCpPage() {
     }
   });
 }
+
+// CP 의견 찬성 투표 함수
+async function voteInFavor() {
+  try {
+    const response = await axios.post(`/cp/${route.params.cpVersionSeq}/cpOpinionLocation/${route.params.cpOpinionLocationSeq}/${route.params.cpOpinionSeq}/cpOpinionVote`,
+        true,
+        {
+          headers: {
+            Authorization: `Bearer ${authStore.accessToken}`,
+            'Content-Type': 'application/json', // Content-Type 설정
+          }
+        }
+    );
+
+    if (response.status === 200 || response.status === 201) {
+      console.log("찬성 투표를 완료하였습니다.");
+    } else {
+      throw new Error("찬성 투표 중 에러가 발생했습니다.");
+    }
+  } catch (error) {
+    console.error("Error:", error.response ? error.response.data : error.message);
+  }
+}
+
+
+// CP 의견 반대 투표 함수
+async function voteAgainst() {
+  try {
+    const response = await axios.post(`/cp/${route.params.cpVersionSeq}/cpOpinionLocation/${route.params.cpOpinionLocationSeq}/${route.params.cpOpinionSeq}/cpOpinionVote`,
+        false,
+        {
+          headers: {
+            Authorization: `Bearer ${authStore.accessToken}`,
+            'Content-Type': 'application/json', // Content-Type 설정
+          }
+        }
+    );
+
+    if (response.status === 200 || response.status === 201) {
+      console.log("찬성 투표를 완료하였습니다.");
+    } else {
+      throw new Error("찬성 투표 중 에러가 발생했습니다.");
+    }
+  } catch (error) {
+    console.error("Error:", error.response ? error.response.data : error.message);
+  }
+}
 </script>
 
 <template>
@@ -102,9 +151,11 @@ function moveToCpPage() {
       </div>
       <div class="button-group">
         <svg-icon-button
+            @click="voteInFavor()"
             :svgIcon="`<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-hand-thumbs-up-fill' viewBox='0 0 16 16'><path d='M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z'/></svg>`"
             icon-color="var(--symbol-blue)"/>
         <svg-icon-button
+            @click="voteAgainst()"
             :svgIcon="`<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-hand-thumbs-down-fill' viewBox='0 0 16 16'><path d='M6.956 14.534c.065.936.952 1.659 1.908 1.42l.261-.065a1.38 1.38 0 0 0 1.012-.965c.22-.816.533-2.512.062-4.51q.205.03.443.051c.713.065 1.669.071 2.516-.211.518-.173.994-.68 1.2-1.272a1.9 1.9 0 0 0-.234-1.734c.058-.118.103-.242.138-.362.077-.27.113-.568.113-.856 0-.29-.036-.586-.113-.857a2 2 0 0 0-.16-.403c.169-.387.107-.82-.003-1.149a3.2 3.2 0 0 0-.488-.9c.054-.153.076-.313.076-.465a1.86 1.86 0 0 0-.253-.912C13.1.757 12.437.28 11.5.28H8c-.605 0-1.07.08-1.466.217a4.8 4.8 0 0 0-.97.485l-.048.029c-.504.308-.999.61-2.068.723C2.682 1.815 2 2.434 2 3.279v4c0 .851.685 1.433 1.357 1.616.849.232 1.574.787 2.132 1.41.56.626.914 1.28 1.039 1.638.199.575.356 1.54.428 2.591'/></svg>`"
             icon-color="var(--symbol-blue)"/>
         <button class="action-button" @click="goToEditPage">수정</button>
