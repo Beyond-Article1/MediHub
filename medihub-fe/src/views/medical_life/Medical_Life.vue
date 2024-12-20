1028d207c557d806951efda864dc902555b05a7a
SANGHYUN0519 <tkdgus0519@naver.com>
2024-12-20 09:03:02 +0900

    REFACTOR: 페이지네이션

--- medihub-fe/src/views/medical_life/Medical_Life.vue
+++ medihub-fe/src/views/medical_life/Medical_Life.vue
@@ -79,7 +79,7 @@
       </div>
 
       <!-- 페이지네이션 -->
-      <Pagenation
+      <Pagination
           :totalData="filteredPosts.length"
           :limitPage="itemsPerPage"
           :page="currentPage"
@@ -91,9 +91,9 @@
 
 <script setup>
 import { ref, computed, onMounted } from "vue";
-import Pagenation from "@/components/common/Pagenation.vue";
 import LineDivider from "@/components/common/LineDivider.vue";
 import axios from "axios";
+import Pagination from "@/components/common/Pagination.vue";
 
 // 상태 변수
 const departments = ref([]);
