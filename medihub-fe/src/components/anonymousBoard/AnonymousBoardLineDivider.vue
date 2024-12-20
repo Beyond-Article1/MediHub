5c09a2392e3106403e35a2513be8e2aaf0823daa
ygc1994 <ygc1994@naver.com>
2024-12-20 12:36:02 +0900

    feat #46: 익명 게시판, Elasticsearch 화면 기능 구현

---
+++ medihub-fe/src/components/anonymousBoard/AnonymousBoardLineDivider.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <hr class="custom-hr">
+  </div>
+</template>
+
+<style scoped>
+.custom-hr {
+  border-style: solid;
+  height: 1px;
+  background-color: #1C306A;
+}
+</style>
