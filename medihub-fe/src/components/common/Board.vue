3b63f3fdbca92b5788e8b61eb5c78564e3698a2e
Pangtaek <zombie3632@gmail.com>
2024-12-20 18:57:58 +0900

    chore: 불필요 CSS 제거

--- medihub-fe/src/components/common/Board.vue
+++ medihub-fe/src/components/common/Board.vue
@@ -10,12 +10,11 @@
 
 <style scoped>
 .board {
-  gap: 1rem; /* 카드 간의 간격 */
   color: #333; /* 텍스트 색상 */
   display: flex;
   width: 1000px;
   height: 540px;
-  background-color: #f3f2f2; /* 부드러운 연한 파란색 배경 */
+  background-color: var(--light-gray); /* 부드러운 연한 파란색 배경 */
   border: 1px solid #ccc; /* 경계선 추가 */
   border-radius: 10px; /* 모서리 둥글게 */
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1); /* 그림자 효과 */
