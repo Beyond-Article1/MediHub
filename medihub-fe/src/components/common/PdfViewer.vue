78c32de17b9d0ad865c2bd0af125f67b3aadb6b1
Pangtaek <zombie3632@gmail.com>
2024-12-30 20:54:06 +0900

    chore: 주석 위치 변경

--- medihub-fe/src/components/common/PdfViewer.vue
+++ medihub-fe/src/components/common/PdfViewer.vue
@@ -134,8 +134,8 @@ const handlePdfClick = (event) => {
     };
     isModalVisible.value = true; // 모달 열기
   } else {
-    console.log("새로운 위치 입니다.");
     if (isMarkerEnabled.value === true) {
+      console.log("새로운 위치 입니다.");
       addMarker(x, y);
       clickedMarkerData.value = {x, y, cpOpinionLocationSeq: -1};
       isModalVisible.value = true; // 모달 열기
